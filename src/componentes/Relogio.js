204929cc9a55c20260ec208776fa245483629bf2
limasuu <szlima.93@gmail.com>
2023-01-25 10:31:52 -0300

    Personalizando minuto final

--- src/componentes/Relogio.js
+++ src/componentes/Relogio.js
@@ -3,6 +3,15 @@ import {connect} from 'react-redux';
 
 function Relogio({sessao, tempo}){
 
+    if(document.querySelector('#relogio') !== null){
+        const classesRelogio= document.querySelector('#relogio').classList;
+      
+        if(tempo.substring(0,2) === '00')
+            classesRelogio.add('minuto-final');
+        else if(tempo.substring(0,2) !== '00' && classesRelogio.contains('minuto-final'))
+            classesRelogio.remove('minuto-final');
+    }
+
     return (
         <div id='relogio'>
             <h3 id='timer-label'>{sessao ? 'Session' : 'Break'}</h3>
